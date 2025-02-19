03b459a966d2175dbb2baa53c604ea97f4f028be
Edgar <hhhedgar316@gmail.com>
2025-02-19 16:39:02 -0600

    Add User, UserRepository and UserService

--- backend/src/main/java/com/back/banka/Repository/UserRepository.java
+++ backend/src/main/java/com/back/banka/Repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.back.banka.Repository;
 
-public interface UserRepository {
+import java.util.Optional;
+import com.back.banka.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
 }
--- backend/src/main/java/com/back/banka/Services/Impl/UserServiceImpl.java
+++ backend/src/main/java/com/back/banka/Services/Impl/UserServiceImpl.java
@@ -1,4 +1,31 @@
 package com.back.banka.Services.Impl;
+import com.back.banka.Model.User;
+import com.back.banka.Repository.UserRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User obtenerUsuarioPorId(Long id){
+        return userRepository.findById(id).orElseThrow(()-> new RuntimeException("Usuario no encontrado"));
+    }
+
+    public User bloquearUsuario(Long id){
+        User usuario = obtenerUsuarioPorId(id);
+        usuario.setStatus(false);
+        return userRepository.save(usuario);
+    }
+
+    public User desbloquearUsuario(Long id){
+        User usuario = obtenerUsuarioPorId(id);
+        usuario.setStatus(true);
+        return userRepository.save(usuario);
+    }
+
 }
