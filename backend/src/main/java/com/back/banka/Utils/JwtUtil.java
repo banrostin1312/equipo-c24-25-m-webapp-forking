72cf1be2bc3dc0ab9bab2deb537fccc8aa037eda
SoyNar <narciris.tech@gmail.com>
2025-02-20 17:57:12 -0500

    add changes about authentication

--- backend/src/main/java/com/back/banka/Utils/JwtUtil.java
+++ backend/src/main/java/com/back/banka/Utils/JwtUtil.java
@@ -1,51 +1,61 @@
-//package com.back.banka.Utils;
-//
-//import io.jsonwebtoken.*;
-//import io.jsonwebtoken.security.Keys;
-//import org.springframework.stereotype.Component;
-//
-//import java.security.Key;
-//import java.util.Date;
-//
-//@Component
-//public class JwtUtil {
-//
-//    private final String SECRET_KEY= "secret_key";
-//
-//    private final long EXPIRATION_TIME = 1000*60*60;
-//
-//    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
-//
-//
-//    public String generateToken(String email){
-//        return Jwts.builder()
-//                .setSubject(email)
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))// 1 hora
-//                .signWith(key, SignatureAlgorithm.HS256).compact();
-//    }
-//
-//    public String extractEmail(String token){
-//        return Jwts.parserBuilder()
-//                .setSigningKey(key)
-//                .build()
-//                .parseClaimsJws(token)
-//                .getBody()
-//                .getSubject();
-//    }
-//
-//    public boolean validateToken(String token){
-//        try{
-//            Jwts.parserBuilder()
-//                    .setSigningKey(key)
-//                    .build()
-//                    .parseClaimsJws(token);
-//            return true;
-//        } catch (ExpiredJwtException e){
-//            System.out.println("Token expired:" + e.getMessage());
-//        } catch (JwtException e){
-//            System.out.println("Invalid token" + e.getMessage());
-//        }
-//        return false;
-//    }
-//}
+package com.back.banka.Utils;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtUtil {
+
+    private final Key key;
+    private final long expirationTime;
+
+    public JwtUtil(@Value("${JWT_SECRET}") String secret, @Value("${JWT_EXPIRATION}")long expiration){
+        this.key = Keys.hmacShaKeyFor(secret.getBytes()); //Generando clave con HS256
+        this.expirationTime = expiration;
+    }
+
+
+    public String generateToken(String email){
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())//Fecha de creación del token
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))// Tiempo en que va a expirar el Token
+                .signWith(key, SignatureAlgorithm.HS256)//Se usa HS256
+                .compact();
+    }
+
+    public String extractEmail(String token){
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            getClaims(token);
+            return true;
+        } catch (ExpiredJwtException e){
+            System.out.println("Token expirado:" + e.getMessage());
+        } catch (JwtException e){
+            System.out.println("Token Inválido" + e.getMessage());
+        }
+        return false;
+    }
+
+    //Para la validación del token
+
+    public String ValidateTokenAndGetMessage(String token){
+        return validateToken(token)?"Token válido":"Token inválido";
+    }
+
+    public Claims getClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
