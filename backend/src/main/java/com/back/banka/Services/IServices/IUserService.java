866f8b261ddffba9b425b12fadaf77f606ee067b
Nar Mena <narciris.tech@gmail.com>
2025-02-19 21:19:03 -0500

    feat:add changes about properties

--- backend/src/main/java/com/back/banka/Services/IServices/IUserService.java
+++ backend/src/main/java/com/back/banka/Services/IServices/IUserService.java
@@ -1,4 +1,16 @@
 package com.back.banka.Services.IServices;
 
+import com.back.banka.Dtos.RequestDto.LoginRequestDto;
+import com.back.banka.Dtos.ResponseDto.LoginResponseDto;
+
 public interface IUserService {
+
+    /**
+     * metodo para autenticacion de un usuario
+     * se valida a traves del username y la password
+     * se devuelve token de autenticacion(JWT)
+     * @return LoginResponseDto
+     * */
+    LoginResponseDto authenticate(LoginRequestDto loginRequestDto);
+
 }
--- backend/src/main/java/com/back/banka/Services/Impl/UserServiceImpl.java
+++ backend/src/main/java/com/back/banka/Services/Impl/UserServiceImpl.java
@@ -1,15 +1,22 @@
 package com.back.banka.Services.Impl;
+import com.back.banka.Dtos.RequestDto.LoginRequestDto;
+import com.back.banka.Dtos.ResponseDto.LoginResponseDto;
 import com.back.banka.Repository.UserRepository;
 
+import com.back.banka.Services.IServices.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements IUserService {
 
 
     @Autowired
     private UserRepository userRepository;
 
 
+    @Override
+    public LoginResponseDto authenticate(LoginRequestDto loginRequestDto) {
+        return null;
+    }
 }
