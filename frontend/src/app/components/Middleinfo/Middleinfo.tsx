bbdd438f3e0e1f93f357300daabaac6de2305be3
Andres Rodriguez <banrostin@gmail.com>
2025-02-22 16:00:50 -0600

    Ajustes px images

--- frontend/src/app/components/Middleinfo/Middleinfo.tsx
+++ frontend/src/app/components/Middleinfo/Middleinfo.tsx
@@ -8,7 +8,7 @@ const Middleinfo: React.FC = () => {
 
     <div>
       <section className="mt-12 md:space-y-20">
-        <div className="flex flex-col md:flex-row justify-center items-center space-x-4 md:space-x-52 ">
+        <div className="flex flex-col md:flex-row justify-center items-center space-x-4 md:space-x-32 ">
           <Image height={126} width={126} src="/persona3.png" alt="img1 middle" className="md:w-[226px] md:h-[226px]"></Image>
           <div className="flex flex-col justify-center items-center space-y-3 md:flex-row ">
             <div className="flex flex-col justify-center items-center space-y-3">
@@ -20,7 +20,7 @@ const Middleinfo: React.FC = () => {
 
         </div>
 
-        <div className="flex flex-col md:flex-row justify-center items-center space-x-4 md:space-x-52 mt-9">
+        <div className="flex flex-col md:flex-row justify-center items-center space-x-4 md:space-x-32 mt-9">
           <Image height={126} width={126} src="/persona4.png" alt="img1 middle" className="md:w-[226px] md:h-[226px]"></Image>
           <div className="flex flex-col justify-center items-center space-y-3 md:flex-row ">
             <div className="flex flex-col justify-center items-center space-y-3">
--- frontend/src/app/components/Separacionseccion/Separacionseccion.tsx
+++ frontend/src/app/components/Separacionseccion/Separacionseccion.tsx
@@ -3,10 +3,10 @@
 const Separacionseccion: React.FC = () => {
     return (
         <div className="mt-8">
-            <div className="bg-black-green-gradient h-[190px] flex flex-col justify-center items-center md:flex-row space-y-2 md:space-y-0 md:space-x-32">
+            <div className="bg-black-green-gradient h-[190px] md:h-[140px] flex flex-col justify-center items-center md:flex-row space-y-2 md:space-y-0 md:space-x-32">
                 <p className="text-white font-sans md:text-2xl">conoce todo lo que tenemos para brindarte</p>
                 <hr className="w-full border-green-hover border-2 md:hidden" />
-                <div className="hidden md:block w-[2px] h-36 bg-green-hover"></div>
+                <div className="hidden md:block w-[2px] h-24 bg-green-hover"></div>
                 <div className="flex flex-row justify-center items-center space-x-4">
                     <div className="flex flex-col justify-center items-center">
                       <img src="/payment-icon.svg" alt="payment icon" className="w-12 hover:invert" />
