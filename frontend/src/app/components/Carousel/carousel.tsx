1e73c8f76781ccee3f7a7f92a74b421372204079
Andres Rodriguez <banrostin@gmail.com>
2025-02-23 06:38:34 -0600

    carousel slider

--- frontend/src/app/components/Carousel/carousel.tsx
+++ frontend/src/app/components/Carousel/carousel.tsx
@@ -1,20 +1,70 @@
-"use client"
-//Libraries 
-//Assets
+"use client";
+import { useState } from "react";
 import Image from "next/image";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
 
-const Swipercarousel: React.FC = () => {
-    return (
-        <div className="flex justify-center items-center mt-7"> 
-            <Swiper spaceBetween={10} slidesPerView={1} loop={true} className="md:w-full md:h-[430px] rounded-2xl h-[130px] w-[99%]">
-                <SwiperSlide><Image alt="flayer1" src="/flayer1.png" objectFit="cover" layout="fill"></Image></SwiperSlide>
-                <SwiperSlide><Image  objectFit="cover" layout="fill" src="/flayer 2.png" alt=""></Image></SwiperSlide>
-                <SwiperSlide><Image  objectFit="cover" layout="fill" src="/flayer 3.png" alt=""></Image></SwiperSlide>
-            </Swiper>
-        </div>
-    )
-}
+const MyCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    "/flayer1.png",
+    "/flayer2.png",
+    "/flayer3.png", // Si tienes más imágenes, agrégalas aquí
+  ];
 
-export default Swipercarousel
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  return (
+    <div id="default-carousel" className="relative w-full" data-carousel="slide">
+      {/* Carousel wrapper */}
+      <div className=" h-56 overflow-hidden rounded-lg md:h-[500px]">
+        {/* Current Image with transition effect */}
+        {images.map((src, index) => (
+          <div
+            key={index}
+            className={`absolute w-full h-full transition-opacity duration-700 ease-in-out ${
+              currentIndex === index ? "opacity-100" : "opacity-0"
+            }`}
+            data-carousel-item
+          >
+            <Image
+              src={src}
+              width={1200}
+              height={406}
+              alt={`Slide ${index + 1}`}
+              className="absolute block object-cover w-full"
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* Slider controls */}
+      <button
+        type="button"
+        className="absolute top-1/2 left-0 z-20 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none transform -translate-y-1/2"
+        onClick={handlePrev}
+      >
+        <span className="inline-flex items-center justify-center  w-6 h-6 md:w-10 md:h-10 rounded-full bg-white/30 group-hover:bg-white/50 md:mb-20 mb-28">
+          <img src="/arrow-left.svg" alt="arrow left" />
+        </span>
+      </button>
+      <button
+        type="button"
+        className="absolute top-1/2 right-0 z-20 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none transform -translate-y-1/2"
+        onClick={handleNext}
+      >
+        <span className="inline-flex items-center justify-center w-6 h-6 md:w-10 md:h-10 rounded-full bg-white/30 group-hover:bg-white/50 md:mb-20 mb-28">
+          <img src="/arrow-right.svg" alt="arrow right" />
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default MyCarousel;
